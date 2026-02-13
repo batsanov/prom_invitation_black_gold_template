942434e8331506f4c8e1f44f390417c21aa53e3d
Toshko Batsanov <totobackn@gmail.com>
2026-02-13 15:49:24 +0200

    template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
-import SecurityLayer from './components/SecurityLayer';
 
 function App() {
   return (
     <>
-      <SecurityLayer />
       <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
